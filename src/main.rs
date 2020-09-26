2afa76ecd3f27aa9bcc44b921643b5019abfdc82
TorchedSammy <38820196+TorchedSammy@users.noreply.github.com>
2020-09-26 15:25:07 -0400

    remove wm

--- src/main.rs
+++ src/main.rs
@@ -6,5 +6,5 @@ fn main() {
 	printlnr!("	   {} OS {}{}", Colors::Green, Colors::Reset, os().unwrap());
 	printlnr!("   (\\ /)   {} Kernel {}{}", Colors::Yellow, Colors::Reset, kernel().unwrap());
 	printlnr!("   ( . .)  {} DE {}{}", Colors::Blue, Colors::Reset, de().unwrap());
-	printlnr!("   c({}\"{})({0}\"{1}) {} WM {1}{}", Colors::Red, Colors::Reset, Colors::Magenta, kernel().unwrap());
+	printlnr!("   c({}\"{})({0}\"{1})", Colors::Red, Colors::Reset);
 }
