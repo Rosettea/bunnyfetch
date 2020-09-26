42294062d18b1b538a671d8b894532b9ede3633f
TorchedSammy <38820196+TorchedSammy@users.noreply.github.com>
2020-09-26 11:06:39 -0400

    de and kernel functions, complete base frontend, crosscompile check

--- src/lib.rs
+++ src/lib.rs
@@ -70,6 +70,51 @@ pub fn os() -> Result<String, bool> {
 	}
 }
 
+#[cfg(target_family = "unix")]
+pub fn kernel() -> Result<String, bool> {
+	let output = Command::new("uname")
+		.arg("-r")
+		.output();
+
+	match output {
+		Ok(output) => return Ok(String::from_utf8(output.stdout).unwrap()),
+		Err(_) => return Err(true),
+	}
+}
+
+#[cfg(target_family = "windows")]
+pub fn kernel() -> Result<String, bool> {
+	let output = Command::new("wmic")
+		.args(&["os", "get", "Version"])
+		.output();
+
+	match output {
+		Ok(output_) => {
+			let output = String::from_utf8(output_.stdout).unwrap();
+			let pat: Vec<&str> = output.split_terminator("\r\r\n").collect();
+			let os = pat[1];
+			return Ok(os.trim().to_string())
+		},
+		Err(_) => return Err(true),
+	}
+}
+
+#[cfg(target_family = "unix")]
+pub fn de() -> Result<String, bool> {
+	unimplemented!()
+}
+
+#[cfg(target_family = "windows")]
+pub fn de() -> Result<String, bool> {
+	let os = os().unwrap();
+	let pat: Vec<&str> = os.split_terminator(" ").collect();
+	if pat[1].trim() == "7" {
+		Ok("Aero".to_string())
+	} else {
+		Ok("Metro".to_string())
+	}
+}
+
 impl fmt::Display for Title {
 	fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
 		write!(f, "{}@{}", self.username, self.hostname)
@@ -78,10 +123,10 @@ impl fmt::Display for Title {
 
 #[macro_export]
 macro_rules! printr {
-    () => (print!("\x1b[0m"));
-    ($($arg:tt)*) => ({
-        print!("{}\x1b[0m", format_args!($($arg)*));
-    })
+	() => (print!("\x1b[0m"));
+	($($arg:tt)*) => ({
+		print!("{}\x1b[0m", format_args!($($arg)*));
+	})
 }
 
 #[macro_export]
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,10 @@
 use bunnyfetch::*;
 
 fn main() {
-	printlnr!("      {}{}", Colors::Red, title());
+	// TODO: make this cleaner (posible use of a macro perhaps?)
+	printr!("	    {}{}", Colors::Red, title());
+	printlnr!("	   {} OS {}{}", Colors::Green, Colors::Reset, os().unwrap());
+	printlnr!("   (\\ /)   {} Kernel {}{}", Colors::Yellow, Colors::Reset, kernel().unwrap());
+	printlnr!("   ( . .)  {} DE {}{}", Colors::Blue, Colors::Reset, de().unwrap());
+	printlnr!("   c({}\"{})({0}\"{1}) {} WM {1}{}", Colors::Red, Colors::Reset, Colors::Magenta, kernel().unwrap());
 }
