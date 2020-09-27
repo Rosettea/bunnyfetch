c117f266bafda42019ac7cd3c3a3c0383e77734c
TorchedSammy <38820196+TorchedSammy@users.noreply.github.com>
2020-09-27 14:01:01 -0400

    add colors strip

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,15 @@
 use bunnyfetch::*;
 
 fn main() {
-	// TODO: make this cleaner (posible use of a macro perhaps?)
+	// TODO: make this cleaner
 	printr!("	    {}{}", Colors::Red, title());
 	printlnr!("	   {} OS {}{}", Colors::Green, Colors::Reset, os().unwrap());
 	printlnr!("   (\\ /)   {} Kernel {}{}", Colors::Yellow, Colors::Reset, kernel().unwrap());
 	printlnr!("   ( . .)  {} DE {}{}", Colors::Blue, Colors::Reset, de().unwrap());
 	printlnr!("   c({}\"{})({0}\"{1})", Colors::Red, Colors::Reset);
+	print!("\n	   ");
+	for pat in 0..=7 {
+		print!("\x1b[4{}m  ", pat)
+	}
+	printlnr!()
 }
