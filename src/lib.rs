8f939856d53a5522fe239d2614fdd678c24b15d2
Devin Singh <drsingh2518@icloud.com>
2021-03-23 17:37:05 -0500

    chore: formatting

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
-use std::fmt;
 use std::env::var;
+use std::fmt;
 use std::process::Command;
 
 pub mod colors;
@@ -7,124 +7,115 @@ pub use crate::colors::Colors;
 
 #[derive(Debug)]
 pub struct Title {
-	username: String,
-	hostname: String
+    username: String,
+    hostname: String,
 }
 
 pub fn username() -> String {
-	var("USERNAME").unwrap()
+    var("USERNAME").unwrap()
 }
 
 #[cfg(target_family = "unix")]
 pub fn hostname() -> Result<String, ()> {
-	let output = Command::new("hostname")
-		.output();
+    let output = Command::new("hostname").output();
 
-	match output {
-		Ok(output) => return Ok(String::from_utf8(output.stdout).unwrap()),
-		Err(_) => return Err(()),
-	}
+    match output {
+        Ok(output) => return Ok(String::from_utf8(output.stdout).unwrap()),
+        Err(_) => return Err(()),
+    }
 }
 
 #[cfg(target_family = "windows")]
 pub fn hostname() -> Result<String, ()> {
-	let output = Command::new("hostname")
-		.output();
+    let output = Command::new("hostname").output();
 
-	match output {
-		Ok(output) => return Ok(String::from_utf8(output.stdout).unwrap()),
-		Err(_) => return Err(()),
-	}
+    match output {
+        Ok(output) => return Ok(String::from_utf8(output.stdout).unwrap()),
+        Err(_) => return Err(()),
+    }
 }
 
 pub fn title() -> Title {
-	let username = username();
-	let hostname = hostname().unwrap_or("Unknown".to_string());
+    let username = username();
+    let hostname = hostname().unwrap_or("Unknown".to_string());
 
-	Title {
-		username,
-		hostname
-	}
+    Title { username, hostname }
 }
 
 #[cfg(target_family = "unix")]
 pub fn os() -> Result<String, ()> {
-	let output = Command::new("lsb_release")
-		.arg("-sd")
-		.output();
+    let output = Command::new("lsb_release").arg("-sd").output();
 
-	match output {
-		Ok(output) => return Ok(String::from_utf8(output.stdout).unwrap()),
-		Err(_) => return Err(()),
-	}
+    match output {
+        Ok(output) => return Ok(String::from_utf8(output.stdout).unwrap()),
+        Err(_) => return Err(()),
+    }
 }
 
 #[cfg(target_family = "windows")]
 pub fn os() -> Result<String, ()> {
-	let output = Command::new("wmic")
-		.args(&["os", "get", "Caption"])
-		.output();
+    let output = Command::new("wmic")
+        .args(&["os", "get", "Caption"])
+        .output();
 
-	match output {
-		Ok(output_) => {
-			let output = String::from_utf8(output_.stdout).unwrap();
-			let pat: Vec<&str> = output.split_terminator("\r\r\n").collect();
-			let os = pat[1];
-			return Ok(os.trim().to_string().split_off(10))
-		},
-		Err(_) => return Err(()),
-	}
+    match output {
+        Ok(output_) => {
+            let output = String::from_utf8(output_.stdout).unwrap();
+            let pat: Vec<&str> = output.split_terminator("\r\r\n").collect();
+            let os = pat[1];
+            return Ok(os.trim().to_string().split_off(10));
+        }
+        Err(_) => return Err(()),
+    }
 }
 
 #[cfg(target_family = "unix")]
 pub fn kernel() -> Result<String, ()> {
-	let output = Command::new("uname")
-		.arg("-r")
-		.output();
+    let output = Command::new("uname").arg("-r").output();
 
-	match output {
-		Ok(output) => return Ok(String::from_utf8(output.stdout).unwrap()),
-		Err(_) => return Err(()),
-	}
+    match output {
+        Ok(output) => return Ok(String::from_utf8(output.stdout).unwrap()),
+        Err(_) => return Err(()),
+    }
 }
 
 #[cfg(target_family = "windows")]
 pub fn kernel() -> Result<String, ()> {
-	let output = Command::new("wmic")
-		.args(&["os", "get", "Version"])
-		.output();
+    let output = Command::new("wmic")
+        .args(&["os", "get", "Version"])
+        .output();
 
-	match output {
-		Ok(output_) => {
-			let output = String::from_utf8(output_.stdout).unwrap();
-			let pat: Vec<&str> = output.split_terminator("\r\r\n").collect();
-			let os = pat[1];
-			return Ok(os.trim().to_string())
-		},
-		Err(_) => return Err(()),
-	}
+    match output {
+        Ok(output_) => {
+            let output = String::from_utf8(output_.stdout).unwrap();
+            let pat: Vec<&str> = output.split_terminator("\r\r\n").collect();
+            let os = pat[1];
+            return Ok(os.trim().to_string());
+        }
+        Err(_) => return Err(()),
+    }
 }
 
 #[cfg(target_family = "unix")]
 pub fn de() -> Result<String, ()> {
-	Ok("Unknown".to_string())
+    Ok("Unknown".to_string())
 }
 
 #[cfg(target_family = "windows")]
 pub fn de() -> Result<String, ()> {
-	let os = os().unwrap();
-	let pat: Vec<&str> = os.split_terminator(" ").collect();
-	if pat[1].trim() == "7" {
-		Ok("Aero".to_string())
-	} else {
-		Ok("Metro".to_string())
-	}
+    let os = os().unwrap();
+    let pat: Vec<&str> = os.split_terminator(" ").collect();
+    if pat[1].trim() == "7" {
+        Ok("Aero".to_string())
+    } else {
+        Ok("Metro".to_string())
+    }
 }
 
 impl fmt::Display for Title {
-	fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-		write!(f, "{}@{}", self.username, self.hostname)
-	}
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}@{}", self.username, self.hostname)
+    }
 }
 
 #[macro_export]
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,25 @@
 use bunnyfetch::*;
 
 fn main() {
-	// TODO: make this cleaner
-	printr!("	    {}{}", Colors::Red, title());
-	printlnr!("	   {} OS {}{}", Colors::Green, Colors::Reset, os().unwrap());
-	printlnr!("   (\\ /)   {} Kernel {}{}", Colors::Yellow, Colors::Reset, kernel().unwrap());
-	printlnr!("   ( . .)  {} DE {}{}", Colors::Blue, Colors::Reset, de().unwrap());
-	printlnr!("   c({}\"{})({0}\"{1})", Colors::Red, Colors::Reset);
-	print!("\n	   ");
-	for pat in 0..=7 {
-		print!("\x1b[4{}m  ", pat)
-	}
-	printlnr!()
+    // TODO: make this cleaner
+    printr!("	    {}{}", Colors::Red, title());
+    printlnr!("	   {} OS {}{}", Colors::Green, Colors::Reset, os().unwrap());
+    printlnr!(
+        "   (\\ /)   {} Kernel {}{}",
+        Colors::Yellow,
+        Colors::Reset,
+        kernel().unwrap()
+    );
+    printlnr!(
+        "   ( . .)  {} DE {}{}",
+        Colors::Blue,
+        Colors::Reset,
+        de().unwrap()
+    );
+    printlnr!("   c({}\"{})({0}\"{1})", Colors::Red, Colors::Reset);
+    print!("\n	   ");
+    for pat in 0..=7 {
+        print!("\x1b[4{}m  ", pat)
+    }
+    printlnr!()
 }
