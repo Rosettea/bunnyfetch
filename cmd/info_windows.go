c7ef8906e8a365b71eff9a3adc20f66408506b8c
sammyette <38820196+TorchedSammy@users.noreply.github.com>
2021-05-31 06:24:50 -0400

    fix: add imports and use output

--- cmd/info_windows.go
+++ cmd/info_windows.go
@@ -1,15 +1,22 @@
 // +build windows
 
+package cmd
+
+import (
+	"os/exec"
+	"strings"
+)
+
 func OS() string {
 	// wmic should be available on all recent windows versions
-	out, _ := exec.Command("wmic", "os", "get", "caption")
+	out, _ := exec.Command("wmic", "os", "get", "caption").Output()
 	winver := strings.TrimSpace(strings.Split(string(out), "\r\r\n")[1])
 
 	return strings.TrimPrefix(winver, "Microsoft ")
 }
 
 func Kernel() string {
-	out, _ := exec.Command("wmic", "os", "get", "Version")
+	out, _ := exec.Command("wmic", "os", "get", "Version").Output()
 	kernelver := strings.TrimSpace(strings.Split(string(out), "\r\r\n")[1])
 
 	return kernelver
